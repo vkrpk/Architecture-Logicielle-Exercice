bea3500d891cf77e79cf51ac0666dc107ab0aa27
Arthur Vietti <arthur.vietti@90tech.fr>
2024-02-07 14:57:51 +0100

    first commit

---
+++ Architecture/Controller/AccountController.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace;
+namespace Microsoft.EntityFrameworkCore;
+
+public class AccountController
+{
+    
+    public void Withdrawal(string accountNumber, string clientName, int amount)
+    {
+        
+    }
+
+    public void Deposit(string accountNumber, string clientName, int amount)
+    {
+        
+    }
+}
