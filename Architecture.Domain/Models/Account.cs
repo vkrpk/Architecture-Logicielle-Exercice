7bb2f4fb6b2b8ff36a741b34fa4263a7f316798c
DevLaurine <laurine.carlier@stagiairesmns.fr>
2024-02-07 14:43:03 +0100

    Create Abstract class Account + gitignore

--- Architecture.Domain/Models/Account.cs
+++ Architecture.Domain/Models/Account.cs
@@ -1,6 +1,19 @@
 ﻿namespace Architecture.Domain.Models
 {
-    public abstract class Account
+    public abstract class Account : BaseModel
     {
+        protected int Balance;
+        protected string AccountNumber;
+
+        public int Debit(int amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+        public int Credit(int amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
     }
 }
