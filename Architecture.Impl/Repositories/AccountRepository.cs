e2c610c2b051ed4ad04fbbf4d5cc4fd0ab85f916
Arthur Vietti <arthur.vietti@90tech.fr>
2024-02-08 09:27:32 +0100

    Repository

--- Architecture.Impl/Repositories/AccountRepository.cs
+++ Architecture.Impl/Repositories/AccountRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Architecture.Domain.Models;
+using Architecture.Impl.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace Architecture.Impl.Repositories
@@ -9,15 +10,21 @@ namespace Architecture.Impl.Repositories
     public class AccountRepository
     {
         private readonly AppDbContext _context;
+        private IAccountService _accountService;
 
         public AccountRepository(AppDbContext context)
         {
             _context = context;
         }
 
+        public Account getAccountById(Guid id)
+        {
+            return _context.Accounts.Find(id);
+        }
+
         public void Add(Account account)
         {
-            _context.Accounts.Add(account);
+            _context.Add(account);
             _context.SaveChanges();
         }
 
@@ -33,10 +40,7 @@ namespace Architecture.Impl.Repositories
             _context.SaveChanges();
         }
 
-        public Account GetById(Guid id)
-        {
-            return _context.Accounts.Find(id);
-        }
+
 
         public IEnumerable<Account> GetAll()
         {
--- Architecture.Impl/Repositories/NoOverdraftAccountRepository.cs
+++ Architecture.Impl/Repositories/NoOverdraftAccountRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Architecture.Impl.Repositories
 {
-    internal class NoOverdraftAccount: AccountRepository
+    internal class NoOverdraftAccountRepository: AccountRepository
     {
         public override int Debit(int amount)
         {
