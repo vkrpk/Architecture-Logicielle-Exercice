860a73a0a76d4e72d0d3557e5d61521a59ef4454
Arthur Vietti <arthur.vietti@90tech.fr>
2024-02-08 16:35:52 +0100

    Controllers

---
+++ Architecture/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Architecture.Domain.Models;
+
+namespace DefaultNamespace
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly Account _noOverdraftAccount;
+        private readonly Account _overdraftAccount;
+
+        public AccountController()
+        {
+            _noOverdraftAccount = new NoOverdraftAccount();
+            _overdraftAccount = new OverdraftAccount();
+        }
+
+        [HttpGet("{accountId}")]
+        public IActionResult GetAccountById(Guid accountId)
+        {
+            Account account = GetAccountFromDatabase(accountId);
+
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(account);
+        }
+
+        private Account GetAccountFromDatabase(Guid accountId)
+        {
+            Account account = FindAccountInDatabase<NoOverdraftAccount>(accountId);
+            if (account == null)
+            {
+                account = FindAccountInDatabase<OverdraftAccount>(accountId);
+            }
+            return account;
+        }
+
+        public IActionResult DebitAccount(Guid accountId)
+        {
+            Account account = GetAccountById(accountId);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(account);
+        }
+
+        private Account FindAccountInDatabase<T>(Guid accountId) where T : Account
+        {
+            return null;
+        }
+    }
+
+}
