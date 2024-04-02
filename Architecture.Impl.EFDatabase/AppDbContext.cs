74877e0cede84b434a76a0b47fd0817077a39b88
DevLaurine <laurine.carlier@stagiairesmns.fr>
2024-04-02 16:38:20 +0200

    Fix mock db issue

--- Architecture.Impl.EFDatabase/AppDbContext.cs
+++ Architecture.Impl.EFDatabase/AppDbContext.cs
@@ -12,6 +12,7 @@ namespace Architecture.Impl.EFDatabase
         public DbSet<NoOverdraftAccount> NoOverdraftAccounts { get; set; }
         public DbSet<Customer> Customers { get; set; }
 
+        public AppDbContext() { }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             Banks = Set<Bank>();
